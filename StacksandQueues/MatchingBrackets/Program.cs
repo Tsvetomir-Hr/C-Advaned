c4a4e2dbb6000b47503c7137b054240e710d6672
Tsvetomir <cecohristov97@gmail.com>
2022-05-25 22:12:41 +0300

    MatchingBrackets

---
+++ StacksandQueues/MatchingBrackets/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            string inBrackets = string.Empty;
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+
+
+                    inBrackets = input.Substring(stack.Peek(), i - stack.Pop() + 1);
+                    Console.WriteLine(inBrackets);
+                }
+            }
+
+
+        }
+    }
+}
