2fa2167538e587d62c6a1372d1956af66db5592a
Tsvetomir <cecohristov97@gmail.com>
2022-05-25 22:18:43 +0300

    SuperMarket

---
+++ StacksandQueues/SuperMarket/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string customer = Console.ReadLine();
+
+            Queue<string> queue = new Queue<string>();
+            while (customer != "End")
+            {
+                if (customer == "Paid")
+                {
+                    foreach (var client in queue)
+                    {
+                        Console.WriteLine(client);
+                    }
+                    queue.Clear();
+                    customer = Console.ReadLine();
+                    continue;
+                }
+                queue.Enqueue(customer);
+
+                customer = Console.ReadLine();
+
+            }
+
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+
+        }
+    }
+}
