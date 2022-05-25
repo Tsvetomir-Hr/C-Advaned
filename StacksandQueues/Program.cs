3e143d13b26e27469a1d0a8f147a33fc70cba506
Tsvetomir <cecohristov97@gmail.com>
2022-05-25 21:39:06 +0300

    P01.StacksandQueues

---
+++ StacksandQueues/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace p01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char ch in input)
+            {
+                stack.Push(ch);
+
+            }
+
+
+            foreach (char ch in stack)
+            {
+                Console.Write(ch);
+            }
+        }
+    }
+}
