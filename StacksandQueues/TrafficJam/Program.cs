b7d005d830189231a9db6421a9f62c1bb36a4b91
Tsvetomir <cecohristov97@gmail.com>
2022-05-25 22:25:10 +0300

    TrafficJam

---
+++ StacksandQueues/TrafficJam/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOfCars = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+
+            string input = Console.ReadLine();
+
+            int passedCars = 0;
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    if (queue.Count == 0)
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    for (int i = 0; i < numberOfCars; i++)
+                    {
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        passedCars++;
+                        if (queue.Count == 0)
+                        {
+                            break;
+                        }
+                    }
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                queue.Enqueue(input);
+
+                input = Console.ReadLine();
+
+            }
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+
+
+
+
+
+        }
+    }
+}
