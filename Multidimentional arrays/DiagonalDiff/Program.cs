dcfaeba09cef194f686c75eeb3715a68ac285463
Tsvetomir <cecohristov97@gmail.com>
2022-05-26 22:59:11 +0300

    DiagonalDiff and SquaresInMatrix problems

---
+++ Multidimentional arrays/DiagonalDiff/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Matrix.Exersise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputNumbers[col];
+                }
+            }
+            int leftRightSum = 0;
+            int rightToLeft = 0;
+            //left to right
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                leftRightSum += matrix[i, i];
+                rightToLeft += matrix[i, matrix.GetLength(1) - 1 - i];
+            }
+            //right to left
+            Console.WriteLine(Math.Abs(leftRightSum-rightToLeft));
+        }
+    }
+}
---
+++ Multidimentional arrays/SquaresInMatrix/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.Exersise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowCol = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[rowCol[0], rowCol[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] chars = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = chars[col];
+
+                }
+            }
+            int squareCounter = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col + 1] == matrix[row, col] &&
+                        matrix[row + 1, col] == matrix[row, col]
+                        && matrix[row + 1, col + 1] == matrix[row, col])
+                    {
+                        squareCounter++;
+                    }
+
+                }
+            }
+            Console.WriteLine(squareCounter);
+        }
+
+    }
+}
