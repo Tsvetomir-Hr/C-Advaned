587d023e51c688082b4a134dac3a90b4a414658c
Tsvetomir <cecohristov97@gmail.com>
2022-05-26 22:51:41 +0300

    SumMatrixCols

---
+++ Multidimentional arrays/SumOfCols/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfCols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixColsAndRows = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = matrixColsAndRows[0];
+            int cols = matrixColsAndRows[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowInput[col];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sumCol = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sumCol+=matrix[row, col];
+                    //Console.Write($" {matrix[row,col]}");
+                }
+                Console.WriteLine(sumCol);
+            }
+        }
+    }
+}
