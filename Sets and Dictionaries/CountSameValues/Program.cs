c8548f98ed932abe5fa6da59471fecaab9e7436d
Tsvetomir <cecohristov97@gmail.com>
2022-05-27 23:27:59 +0300

    CountSameValues

---
+++ Sets and Dictionaries/CountSameValues/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            Dictionary<double, int> numberOccurences = new Dictionary<double, int>();
+            foreach (var number in numbers)
+            {
+                if (!numberOccurences.ContainsKey(number))
+                {
+                    numberOccurences.Add(number, 0);
+                }
+                numberOccurences[number]++;
+            }
+
+            foreach (var occurance in numberOccurences)
+            {
+                Console.WriteLine($"{occurance.Key} - {occurance.Value} times");
+            }
+
+        }
+    }
+}
