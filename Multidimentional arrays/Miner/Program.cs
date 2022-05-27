1d5827208475f2b07d750e18201ac9cd0f321205
Tsvetomir <cecohristov97@gmail.com>
2022-05-27 23:24:05 +0300

    Miner game

---
+++ Multidimentional arrays/Miner/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+
+namespace Miner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            string[] directions = Console.ReadLine().Split();
+
+            char[,] matrix = new char[size, size];
+
+            int coalsCount = 0;
+            int playerRow = 0;
+            int playerCol = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+
+                    if (matrix[row, col] == 'c')
+                    {
+                        coalsCount++;
+                    }
+                    if (matrix[row, col] == 's')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+
+                foreach (var direction in directions)
+                {
+                    int nextRow = 0;
+                    int nextCol = 0;
+                    if (direction == "left")
+                    {
+                        nextCol = -1;
+
+                    }
+                    else if (direction == "right")
+                    {
+                        nextCol = 1;
+                    }
+                    else if (direction == "down")
+                    {
+                        nextRow = 1;
+                    }
+                    else if (direction == "up")
+                    {
+                        nextRow = -1;
+                    }
+                    if (!IsInRange(matrix, playerRow + nextRow, playerCol + nextCol))
+                    {
+                        continue;
+                    }
+
+                    playerRow += nextRow;
+                    playerCol += nextCol;
+
+                    if (matrix[playerRow, playerCol] == 'e')
+                    {
+                        Console.WriteLine($"Game over! ({playerRow}, {playerCol})");
+                        Environment.Exit(0);
+                    }
+                    if (matrix[playerRow, playerCol] == 'c')
+                    {
+                        matrix[playerRow, playerCol] = '*';
+                        coalsCount--;
+                    }
+                    if (coalsCount == 0)
+                    {
+                        Console.WriteLine($"You collected all coals! ({playerRow}, {playerCol})");
+                        Environment.Exit(0);
+
+                    }
+                }
+
+            Console.WriteLine($"{coalsCount} coals left. ({playerRow}, {playerCol})");
+        }
+        private static bool IsInRange(char[,] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) &&
+                 col >= 0 && col < matrix.GetLength(1);
+        }
+    }
+}
