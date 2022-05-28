500dc3b6fa7c975172e4fc584cbfe39582edc2e4
Tsvetomir <cecohristov97@gmail.com>
2022-05-28 23:00:10 +0300

    CountSymbols

---
+++ Sets and Dictionaries/CountSymbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Dictionary<char,int> symbolCounts = new Dictionary<char,int>();
+
+            string text= Console.ReadLine();
+
+            foreach (char c in text)
+            {
+                if (!symbolCounts.ContainsKey(c))
+                {
+                    symbolCounts.Add(c, 0);
+                }
+                symbolCounts[c]++;
+            }
+
+            foreach (var kvp in symbolCounts.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
