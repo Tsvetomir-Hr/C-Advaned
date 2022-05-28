a22f874b5416bd25364b6ed0b563cbc9ea6a388a
Tsvetomir <cecohristov97@gmail.com>
2022-05-28 22:58:21 +0300

    Folder repair and adding new

---
+++ Sets and Dictionaries/PeriodicTable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+           Dictionary<int, int> allNumbers = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+
+                if (!allNumbers.ContainsKey(number))
+                {
+                    allNumbers.Add(number, 0);
+                }
+                allNumbers[number] += 1;
+
+            }
+
+            foreach (var number in allNumbers)
+            {
+                if (number.Value%2==0)
+                {
+                    Console.WriteLine(number.Key);
+                    break;
+                }
+            }
+
+         
+        }
+    }
+}
