26b1fbd777e15d7a0bdd0aca715315349aa611f0
Tsvetomir <cecohristov97@gmail.com>
2022-05-29 23:59:46 +0300

    MinNumberWithFunc

---
+++ Functinal Programming Basic/MinNumberWithFunc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace MinNumberWithFunc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int[], int> smallest = nums
+                 =>
+             {
+                 int minValue = int.MaxValue;
+
+                 foreach (int number in nums)
+                 {
+                     if (number < minValue)
+                     {
+                         minValue = number;
+                     }
+                 }
+                 return minValue;
+             };
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(smallest(numbers));
+        }
+    }
+}
