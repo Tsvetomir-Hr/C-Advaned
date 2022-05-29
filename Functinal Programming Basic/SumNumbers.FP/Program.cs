c042e97f0e4e4a62eeadb0962548c01a1803f992
Tsvetomir <cecohristov97@gmail.com>
2022-05-29 23:52:39 +0300

    SumNumbers

---
+++ Functinal Programming Basic/SumNumbers.FP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers.FP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
