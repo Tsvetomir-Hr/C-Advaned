c782c64aaddd6096486d6f5b3082eba8b5e49e85
Tsvetomir <cecohristov97@gmail.com>
2022-05-29 23:54:59 +0300

    AddVAT

---
+++ Functinal Programming Basic/AddVAT/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<decimal> numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(n=>n+n*0.2m)
+                .ToList();
+
+            numbers.ForEach(n=>Console.WriteLine($"{n:F2}"));
+
+            
+
+        }
+    }
+}
