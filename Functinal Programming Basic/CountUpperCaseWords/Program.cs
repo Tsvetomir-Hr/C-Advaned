e42a6dc64b6c267d7428fba01638cc0fbe0d1f6d
Tsvetomir <cecohristov97@gmail.com>
2022-05-29 23:53:52 +0300

    CountUpperCaseWords

---
+++ Functinal Programming Basic/CountUpperCaseWords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CountUpperCaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
