e7803046fbb8864324a3b642815bb63ee20455dc
Tsvetomir <cecohristov97@gmail.com>
2022-05-30 23:10:54 +0300

    PredicateForNames

---
+++ Functinal Programming Basic/PredicateForNames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string,int,bool> isLessOrEqual = (name,length) =>
+            name.Length<= length;
+
+            string[] result = names
+                .Where(x => isLessOrEqual(x, n))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine,result));
+                
+
+
+        }
+    }
+}
