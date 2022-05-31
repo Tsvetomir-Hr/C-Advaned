ad9cdd62db7a88d67d1ef4e93a3dceba46e8e588
Tsvetomir <cecohristov97@gmail.com>
2022-05-31 23:09:16 +0300

    CarConstrictor

---
+++ ClassesBasic/CarConstrictor/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    internal class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make,string model,int year):this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+
+        }
+        public Car(string make, string model, int year,int fuelQuantity , int fuelConsumption) : this(make,model,year)
+        {
+            FuelConsumption = fuelConsumption;
+            FuelQuantity = fuelQuantity;
+
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make} Model: { this.Model} Year: { this.Year} Fuel: { this.FuelQuantity:F2}";
+        }
+    }
+}
---
+++ ClassesBasic/CarConstrictor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car defaultCar = new Car();
+
+           
+
+            Console.WriteLine(defaultCar.WhoAmI());
+        }
+    }
+}
