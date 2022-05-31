ff274480534781c2127a285cd6e63e37ad72e9df
Tsvetomir <cecohristov97@gmail.com>
2022-05-31 23:07:26 +0300

    CarExtension

---
+++ ClassesBasic/CarExtension/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        public string  Make { get; set; }
+        public string  Model { get; set; }
+        public int  Year { get; set; }
+        public double  FuelQuantity { get; set; }
+        public double  FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - distance * FuelConsumption > 0)
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make} Model: { this.Model} Year: { this.Year} Fuel: { this.FuelQuantity:F2}";
+        }
+
+    }
+}
---
+++ ClassesBasic/CarExtension/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Car car = new Car();
+            car.Make = "Audi";
+            car.Model = "A5";
+            car.Year = 1995;
+            car.FuelQuantity = 150;
+            car.FuelConsumption = 2;
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Driving 50:");
+                car.Drive(40);
+
+                Console.WriteLine(car.WhoAmI());
+            }
+
+        }
+    }
+}
