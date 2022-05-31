192c3479bb985cff051d0ea94c41b5731424e7f1
Tsvetomir <cecohristov97@gmail.com>
2022-05-31 23:05:42 +0300

    Car

---
+++ ClassesBasic/Car/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P01.Car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car bmw = new Car()
+            {
+
+                Name = "3",
+                Company = "BMW",
+                Category = "Cars",
+                Price = 3000
+
+            };
+
+            bmw.Drive(23);
+            bmw.Drive(33);
+
+            Car audi = new Car()
+            {
+                Name = "a3",
+                Company = "Audi",
+                Category = "Category",
+                Price = 5000
+            };
+
+            audi.Drive(45);
+            audi.Drive(55);
+
+
+
+        }
+
+
+    }
+}
