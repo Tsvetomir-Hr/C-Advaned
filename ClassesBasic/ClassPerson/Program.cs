9c587c5c334b8c3982b9f6137e35db7d19076fd5
Tsvetomir <cecohristov97@gmail.com>
2022-06-02 07:53:59 +0300

    ClassPerson

---
+++ ClassesBasic/ClassPerson/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person("Peter", 20);
+
+
+            Person newPerson = new Person();
+
+            newPerson.Name = "George";
+            newPerson.Age = 25;
+
+        }
+    }
+}
