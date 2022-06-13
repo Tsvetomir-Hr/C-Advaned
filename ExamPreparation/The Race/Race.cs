975bd04ddcaf983da427de2f0f62ff5f52fbe0a1
Tsvetomir <cecohristov97@gmail.com>
2022-06-13 22:57:15 +0300

    The Race

---
+++ ExamPreparation/The Race/Race.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    public class Race
+    {
+        public List<Racer> data;
+
+        public Race(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Racer>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => data.Count;
+
+        public void Add(Racer racer)
+        {
+            if (Count < Capacity)
+            {
+                data.Add(racer);
+            }
+        }
+        public bool Remove(string name)
+        {
+            Racer racer = data.FirstOrDefault(x => x.Name == name);
+            if (racer == null)
+            {
+                return false;
+            }
+            data.Remove(racer);
+            return true;
+        }
+        public Racer GetOldestRacer()
+        {
+            return data.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+        public Racer GetRacer(string name)
+        {
+            return data.FirstOrDefault(x => x.Name == name);
+        }
+        public Racer GetFastestRacer()
+        {
+            Car fastestCar = data.Select(x => x.Car).OrderByDescending(x => x.Speed).FirstOrDefault();
+
+
+            return data.FirstOrDefault(x => x.Car == fastestCar);
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Racers participating at {Name}");
+            foreach (var racer in data)
+            {
+                sb.AppendLine(racer.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
