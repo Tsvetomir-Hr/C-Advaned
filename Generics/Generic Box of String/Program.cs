d5d1a863f6ad9837e511e419b95113d41641dd09
Tsvetomir <cecohristov97@gmail.com>
2022-06-17 20:16:10 +0300

    Generic Box of String

---
+++ Generics/Generic Box of String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Items.Add(input);
+            }
+
+            Console.WriteLine(box);
+        }
+    }
+}
