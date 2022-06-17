45e985f4d60cabdb62e8795baa6b0ddce9b84a6f
Tsvetomir <cecohristov97@gmail.com>
2022-06-17 20:58:57 +0300

    Generic Count Method String

---
+++ Generics/Generic Count Method String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Items.Add(input);
+            }
+
+            string comparingItem = Console.ReadLine();
+             int value = box.CountGreaterThan(comparingItem);
+           
+            Console.WriteLine(value);
+        }
+    }
+}
