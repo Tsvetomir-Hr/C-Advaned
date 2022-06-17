6c68ee6d48fb138b245752f0ffe0df99d59fee08
Tsvetomir <cecohristov97@gmail.com>
2022-06-17 20:33:16 +0300

    Generic Swap Method String

---
+++ Generics/Generic Swap Method String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string stringValue = Console.ReadLine();
+                box.Items.Add(stringValue);
+            }
+
+            int[] positions = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            box.Swap(positions[0],positions[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
