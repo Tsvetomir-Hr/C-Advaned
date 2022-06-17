b66ea5f93b646eae4467def2d2757d85a21b9baf
Tsvetomir <cecohristov97@gmail.com>
2022-06-17 20:19:59 +0300

    Generic Box of Integer

---
+++ Generics/Generic Box of Integer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Box<int> box = new Box<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                box.Items.Add(number);
+            }
+
+            Console.WriteLine(box);
+        }
+    }
+}
