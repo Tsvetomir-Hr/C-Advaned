9d4d065f2fceabc12efb5d301744cedec5922e9d
Tsvetomir <cecohristov97@gmail.com>
2022-06-17 21:03:38 +0300

    Generic Count Method Double

---
+++ Generics/Generic Count Method Double/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Box<double> box = new Box<double>();
+            for (int i = 0; i < n; i++)
+            {
+                double a = double.Parse(Console.ReadLine());
+                box.Items.Add(a);
+            }
+
+            double comparingItem = double.Parse(Console.ReadLine());
+            int value = box.CountGreaterThan(comparingItem);
+           
+            Console.WriteLine(value);
+        }
+    }
+}
