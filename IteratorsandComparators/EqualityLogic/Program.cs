3660e54993b5ed1b61758962870e6e6fb95b81a4
Tsvetomir <cecohristov97@gmail.com>
2022-06-19 17:08:49 +0300

    EqualityLogic

---
+++ IteratorsandComparators/EqualityLogic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet <Person> sortedSet = new SortedSet<Person>();
+            HashSet<Person> hashSet = new HashSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine()
+                    .Split();
+                string name  = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                sortedSet.Add(new Person(name, age));
+                hashSet.Add(new Person(name, age));
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+
+        }
+    }
+}
