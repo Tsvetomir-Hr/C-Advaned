088ae12f4143545fa636d74dc23376d883cc64ca
Tsvetomir <cecohristov97@gmail.com>
2022-06-19 23:06:34 +0300

    Parking

---
+++ ExamPreparation/Parking/Parking.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    public class Parking
+    {
+        List<Car> data;
+
+        public Parking(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            data = new List<Car>();
+        }
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public int Count => data.Count;
+
+        public void Add(Car car)
+        {
+            if (Count < Capacity)
+            {
+                data.Add(car);
+            }
+        }
+        public bool Remove(string manufacturer, string model)
+        {
+            Car car = data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+            if (car == null)
+            {
+                return false;
+            }
+            data.Remove(car);
+            return true;
+        }
+        public Car GetLatestCar()
+        {
+            return data.OrderByDescending(x => x.Year).FirstOrDefault();
+        }
+        public Car GetCar(string manufacturer, string model)
+        {
+            return data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+        }
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"The cars are parked in {Type}:");
+            foreach (var car in data)
+            {
+                sb.AppendLine(car.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
