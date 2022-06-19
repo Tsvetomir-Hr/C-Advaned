d52fae1207c7cd2d1d66d818d9efb245010e20d4
Tsvetomir <cecohristov97@gmail.com>
2022-06-19 16:48:19 +0300

    ComparingObjects

---
+++ IteratorsandComparators/ComparingObjects/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string  Town { get; set; }
+
+        public int CompareTo( Person other)
+        {
+            int names = Name.CompareTo(other.Name);
+            if (names!=0)
+            {
+                return names;
+            }
+            int age = Age.CompareTo(other.Age);
+            if (age!=0)
+            {
+                return age;
+            }
+            int town = Town.CompareTo(other.Town);
+            if (town!=0)
+            {
+                return town;
+            }
+            //Name !=
+            //Age 
+            //Town
+            return 0;
+        }
+    }
+}
---
+++ IteratorsandComparators/ComparingObjects/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string info = Console.ReadLine();
+
+            while(info!="END")
+            {
+                string[] personInfo = info.Split();
+
+                string name  = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+                string town  = personInfo[2];
+
+                Person person = new Person(name, age, town);
+                people.Add(person);
+
+
+                info = Console.ReadLine();
+            }
+            int n = int.Parse(Console.ReadLine());
+
+            Person personToCompare = people[n - 1];
+
+            int equals = 0;
+            int diff = 0;
+            foreach (var person in people)
+            {
+                if (person.CompareTo(personToCompare)==0)
+                {
+                    equals++;
+                }
+                else
+                {
+                    diff++;
+                }
+            }
+
+            if (equals==1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+            Console.WriteLine($"{equals} {diff} {people.Count}");
+
+            }
+        }
+    }
+}
