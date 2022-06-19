0e55cf1f75eb119929da13e6bdfa321d200b4e2d
Tsvetomir <cecohristov97@gmail.com>
2022-06-19 15:49:34 +0300

    ListyIterator

---
+++ IteratorsandComparators/ListyIterator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> elements = Console.ReadLine()
+                .Split()
+                .Skip(1)
+                .ToList();
+
+            ListyIterator<string> listyIterator =
+                new ListyIterator<string>(elements);
+            string commands = Console.ReadLine();
+            try
+            {
+                while (commands != "END")
+                {
+                    if (commands == "HasNext")
+                    {
+                        Console.WriteLine(listyIterator.HasNext());
+                    }
+                    else if (commands == "Move")
+                    {
+                        Console.WriteLine(listyIterator.Move());
+                    }
+                    else if (commands == "Print")
+                    {
+                        listyIterator.Print();
+                    }
+                    commands = Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
