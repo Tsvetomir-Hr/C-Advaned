ee50e29314e90fcba9c7224e924ce2087e930185
Tsvetomir <cecohristov97@gmail.com>
2022-06-27 23:40:26 +0300

    Inheritance of List<string>

---
+++ C# OOP/CustomRandomList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            RandomList randomList = new RandomList();
+
+            randomList.Add("Pesho1");
+            randomList.Add("Pesho2");
+            randomList.Add("Pesho3");
+            randomList.Add("Pesho4");
+            randomList.Add("Pesho5");
+
+            Console.WriteLine(randomList.RandomString());
+            Console.WriteLine(randomList.RandomString());
+            Console.WriteLine(randomList.RandomString());
+            Console.WriteLine(randomList.RandomString());
+            Console.WriteLine(randomList.RandomString());
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
