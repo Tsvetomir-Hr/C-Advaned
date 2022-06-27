fb3fc1fcd013e164ef3487a31f75fddf1fe7708d
Tsvetomir <cecohristov97@gmail.com>
2022-06-27 23:42:39 +0300

    new folder files

---
+++ C# OOP/Inheritance/CustomStack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+          
+            StackOfStrings stackOfStrings = new StackOfStrings();
+
+            Console.WriteLine(stackOfStrings.IsEmpty());
+
+            stackOfStrings.AddRange(new List<string>() { "11", "2", "1" });
+
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            stackOfStrings.Pop();
+            stackOfStrings.Pop();
+            stackOfStrings.Pop();
+
+        }
+    }
+}
