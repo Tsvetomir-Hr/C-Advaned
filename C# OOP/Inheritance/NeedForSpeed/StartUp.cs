2cca32b50159bc43625e7a234fabb55cb2638a02
Tsvetomir <cecohristov97@gmail.com>
2022-06-29 00:00:13 +0300

    NeedForSpeed

---
+++ C# OOP/Inheritance/NeedForSpeed/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            CrossMotorcycle sportCar = new CrossMotorcycle(100, 100);
+            sportCar.Drive(9);
+            Console.WriteLine(sportCar.Fuel);
+        }
+    }
+}
