12ce1b8ad310b0441b7f9c4ac87719e9555d964f
Tsvetomir <cecohristov97@gmail.com>
2022-06-28 23:44:56 +0300

    PlayersAndMonsters

---
+++ C# OOP/Inheritance/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            SoulMaster soul = new SoulMaster("Hex", 5);
+            Console.WriteLine(soul);
+        }
+    }
+}
