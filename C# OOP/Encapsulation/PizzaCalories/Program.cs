0936ef7289463d46ed4fe714cb55afb937e3a1c0
Tsvetomir <cecohristov97@gmail.com>
2022-07-08 20:52:10 +0300

    PizzaCalories

---
+++ C# OOP/Encapsulation/PizzaCalories/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pizzaName = Console.ReadLine().Split();
+            string name = pizzaName[1];
+            string[] doughInput = Console.ReadLine().Split();
+
+            string flourType = doughInput[1];
+            string bakingTechnique = doughInput[2];
+            int grams = int.Parse(doughInput[3]);
+
+
+            try
+            {
+                Dough dough = new Dough(flourType, bakingTechnique, grams);
+
+                Pizza pizza = new Pizza(name, dough);
+                string input = Console.ReadLine();
+                while (input != "END")
+                {
+                    string[] toppingInput = input.Split();
+                    string toppingType = toppingInput[1];
+                    int weight = int.Parse(toppingInput[2]);
+
+                    Topping topping = new Topping(toppingType, weight);
+
+                    pizza.AddTopping(topping);
+
+                    input = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{pizza.Name} - {pizza.Calories:F2} Calories.");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+          
+
+        }
+    }
+}
---
+++ C# OOP/Encapsulation/PizzaCalories/Topping.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Topping
+    {
+        private readonly Dictionary<string, double> modifiers =
+            new Dictionary<string, double>()
+            {
+                {"meat", 1.2 },
+                {"veggies", 0.8 },
+                {"cheese", 1.1 },
+                {"sauce", 0.9 }
+            };
+        private int weight;
+        private string toppingType;
+
+        public Topping(string toppingType, int weight)
+        {
+            ToppingType = toppingType;
+            Weight = weight;
+            
+        }
+
+        public string ToppingType
+        {
+            get { return toppingType; }
+            private set
+            {
+                if (!modifiers.ContainsKey(value.ToLower()))
+                {
+                    throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+                }
+                toppingType = value;
+            }
+        }
+
+        public int Weight
+        {
+            get { return weight; }
+            private set
+            {
+                if (value < 1 || value > 50)
+                {
+                    throw new ArgumentException($"{toppingType} weight should be in the range [1..50].");
+                }
+                weight = value;
+            }
+        }
+        public double Calories
+            => 2
+            * Weight
+            * modifiers[toppingType.ToLower()];
+
+
+    }
+}
