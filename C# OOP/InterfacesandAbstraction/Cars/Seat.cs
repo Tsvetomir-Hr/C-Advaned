c261d340d298509dc778b9cebccc742bb43fcc31
Tsvetomir <cecohristov97@gmail.com>
2022-08-15 23:56:25 +0300

    Exercise

---
+++ C# OOP/InterfacesandAbstraction/Cars/Seat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    internal class Seat : ICar
+    {
+        private string v1;
+        private string v2;
+
+        public Seat(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine Start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model}";
+        }
+    }
+}
---
+++ C# OOP/InterfacesandAbstraction/Cars/Tesla.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        private string v1;
+        private string v2;
+        private int v3;
+
+        public Tesla(string v1, string v2, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+
+        public int Battery { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine Start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}";
+        }
+    }
+}
