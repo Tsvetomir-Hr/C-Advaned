35365a43a66ca482b6f1575b3e6ff29cc3de5474
Tsvetomir <cecohristov97@gmail.com>
2022-08-16 23:59:24 +0300

    ExamPrep

---
+++ C# OOP/ExamPreparation/Formula1/Formula1/Formula1/Core/Controller.cs
@@ -0,0 +1,176 @@
+﻿using Formula1.Core.Contracts;
+using Formula1.Models;
+using Formula1.Models.Contracts;
+using Formula1.Repositories;
+using Formula1.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formula1.Core
+{
+    public class Controller : IController
+    {
+        private PilotRepository pilotRepository;
+        private RaceRepository raceRepository;
+        private FormulaOneCarRepository carRepository;
+        public Controller()
+        {
+            pilotRepository = new PilotRepository();
+            raceRepository = new RaceRepository();
+            carRepository = new FormulaOneCarRepository();
+        }
+        public string AddCarToPilot(string pilotName, string carModel)
+        {
+            IPilot pilot = pilotRepository.FindByName(pilotName);
+            IFormulaOneCar car = carRepository.FindByName(carModel);
+            if (pilot == null || pilot.Car != null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.PilotDoesNotExistOrHasCarErrorMessage, pilotName));
+            }
+            if (car == null)
+            {
+                throw new NullReferenceException(string.Format(ExceptionMessages.CarDoesNotExistErrorMessage, carModel));
+            }
+            pilot.AddCar(car);
+            carRepository.Remove(car);
+            return string.Format(OutputMessages.SuccessfullyPilotToCar, pilotName, car.GetType().Name, carModel);
+        }
+
+        public string AddPilotToRace(string raceName, string pilotFullName)
+        {
+            IRace race = raceRepository.FindByName(raceName);
+            IPilot pilot = pilotRepository.FindByName(pilotFullName);
+            if (race == null)
+            {
+                throw new NullReferenceException(string.Format(ExceptionMessages.RaceDoesNotExistErrorMessage, raceName));
+            }
+            if (pilot == null ||
+                pilot.CanRace == false ||
+                race.Pilots.Contains(pilot))
+                //||
+                //raceRepository.Models.All(x => x.Pilots.Contains(pilot))
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.PilotDoesNotExistErrorMessage, pilotFullName));
+            }
+            race.AddPilot(pilot);
+            return string.Format(OutputMessages.SuccessfullyAddPilotToRace, pilotFullName, raceName);
+        }
+
+        public string CreateCar(string type, string model, int horsepower, double engineDisplacement)
+        {
+            IFormulaOneCar car = null;
+            if (type == "Ferrari")
+            {
+                car = new Ferrari(model, horsepower, engineDisplacement);
+            }
+            else if (type == "Williams")
+            {
+                car = new Williams(model, horsepower, engineDisplacement);
+            }
+            else
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InvalidTypeCar, type));
+            }
+            if (carRepository.FindByName(model) != null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.CarExistErrorMessage, model));
+            }
+            carRepository.Add(car);
+            return string.Format(OutputMessages.SuccessfullyCreateCar, type, model);
+
+        }
+
+        public string CreatePilot(string fullName)
+        {
+            IPilot pilot = pilotRepository.FindByName(fullName);
+
+            if (pilot!=null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.PilotExistErrorMessage, fullName));
+            }
+
+            pilotRepository.Add(new Pilot(fullName));
+            return string.Format(OutputMessages.SuccessfullyCreatePilot, fullName);
+        }
+
+        public string CreateRace(string raceName, int numberOfLaps)
+        {
+            IRace race = new Race(raceName, numberOfLaps);
+            if (raceRepository.FindByName(raceName) != null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.RaceExistErrorMessage, raceName));
+            }
+            raceRepository.Add(race);
+            return string.Format(OutputMessages.SuccessfullyCreateRace, raceName);
+        }
+
+        public string PilotReport()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var pilot in pilotRepository.Models)
+            {
+                sb.AppendLine(pilot.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public string RaceReport()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var race in raceRepository.Models.Where(x=>x.TookPlace==true))
+            {
+                sb.AppendLine(race.RaceInfo());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public string StartRace(string raceName)
+        {
+            IRace race = raceRepository.FindByName(raceName);
+
+            if (race == null)
+            {
+                throw new NullReferenceException(string.Format(ExceptionMessages.RaceDoesNotExistErrorMessage, raceName));
+            }
+            if (race.Pilots.Count < 3)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InvalidRaceParticipants, raceName));
+            }
+            if (race.TookPlace == true)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.RaceTookPlaceErrorMessage, raceName));
+            }
+            StringBuilder sb = new StringBuilder();
+
+            var racersOrderedByScore = race.Pilots.OrderByDescending(x => x.Car.RaceScoreCalculator(race.NumberOfLaps));
+            race.TookPlace = true;
+            racersOrderedByScore.First().WinRace();
+
+            int iterationCounter = 0;
+
+            foreach (var pilot in racersOrderedByScore.Take(3))
+            {
+                iterationCounter++;
+                if (iterationCounter==1)
+                {
+                    sb.AppendLine($"Pilot {pilot.FullName} wins the {raceName} race.");
+                    continue;
+                }
+                if (iterationCounter == 2)
+                {
+                    sb.AppendLine($"Pilot {pilot.FullName} is second in the {raceName} race.");
+                    continue;
+                }
+                if (iterationCounter == 3)
+                {
+                    sb.AppendLine($"Pilot {pilot.FullName} is third in the {raceName} race.");
+                    
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/ExamPreparation/Formula1/Formula1/Formula1/Models/Ferrari.cs
@@ -0,0 +1,15 @@
+﻿using Formula1.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public class Ferrari : FormulaOneCar
+    {
+        public Ferrari(string model, int horsepower, double engineDisplacement) 
+            : base(model, horsepower, engineDisplacement)
+        {
+        }
+    }
+}
