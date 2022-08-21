982ebd1001621e41d5ead5da72a608954b7872e9
Tsvetomir <cecohristov97@gmail.com>
2022-08-21 15:04:11 +0300

    EasterRaces exercise

---
+++ C# OOP/ExamPreparation/EasterRaces/EasterRaces/Models/Cars/Entities/Car.cs
@@ -0,0 +1,36 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Cars.Entities
+{
+    public abstract class Car : ICar
+    {
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length<4)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidModel,value,4));
+                }
+                model = value; 
+            }
+        }
+
+
+        public virtual int HorsePower { get; protected set;}
+
+        public double CubicCentimeters => throw new NotImplementedException();
+
+        public double CalculateRacePoints(int laps)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
