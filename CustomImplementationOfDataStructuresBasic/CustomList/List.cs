ea0025a2d30e6a0f9be6e114ddd6c85bfad33e86
Tsvetomir <cecohristov97@gmail.com>
2022-09-04 12:27:30 +0300

    Custom list

---
+++ CustomImplementationOfDataStructuresBasic/CustomList/List.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomList
+{
+    public class List
+    {
+        private const int DefaultCapacity = 2;
+
+        private int[] items;
+        public List()
+        {
+            items = new int[DefaultCapacity];
+        }
+        public int Count { get; private set; }
+
+        public int this[int i]
+        {
+            get
+            {
+                IsInRange(i);
+
+                return items[i];
+            }
+            set
+            {
+                IsInRange(i);
+                items[i] = value;
+            }
+        }
+        public void Add(int element)
+        {
+            if (Count == items.Length)
+            {
+                Resize();
+            }
+            items[Count++] = element;
+
+        }
+        public int RemoveAt(int index)
+        {
+            IsInRange(index);
+
+            int element = items[index];
+
+            items[index] = 0;
+
+            for (int i = index; i < Count; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            Count--;
+            if (Count <= items.Length / 4)
+            {
+                Shrink();
+
+            }
+            return element;
+        }
+
+
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == element)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            if (firstIndex < 0 || firstIndex >= Count
+                || secondIndex < 0 || secondIndex >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            int tempElement = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = tempElement;
+        }
+        private void Shrink()
+        {
+
+            int[] tempArray = new int[items.Length / 2];
+            for (int i = 0; i < Count; i++)
+            {
+                tempArray[i] = items[i];
+            }
+            items = tempArray;
+        }
+        private void Resize()
+        {
+            int[] tempArray = new int[items.Length * 2];
+            for (int i = 0; i < items.Length; i++)
+            {
+                tempArray[i] = items[i];
+            }
+
+            items = tempArray;
+        }
+        private int IsInRange(int i)
+        {
+            if (i < 0 || i >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return items[i];
+        }
+    }
+}
---
+++ CustomImplementationOfDataStructuresBasic/CustomList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CustomList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List list = new List();
+            list.Add(5);
+            list.Add(10);
+            list.Add(125);
+            list.Add(12);
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+          
+            
+        }
+    }
+}
