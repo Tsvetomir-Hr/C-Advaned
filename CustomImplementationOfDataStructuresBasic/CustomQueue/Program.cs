08d6281fcb0cb58db646ba3b8e8dbfc5b85594f0
Tsvetomir <cecohristov97@gmail.com>
2022-09-04 12:28:19 +0300

    Custom queue

---
+++ CustomImplementationOfDataStructuresBasic/CustomQueue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CustomQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> customQueue = new Queue<int>();
+
+            customQueue.Enqueue(10);
+            customQueue.Enqueue(2);
+            customQueue.Enqueue(3);
+            customQueue.Enqueue(4);
+            Console.WriteLine(customQueue.Peek());
+            customQueue.Enqueue(5);
+            customQueue.Enqueue(7);
+            customQueue.Enqueue(11);
+            customQueue.Enqueue(17);
+            customQueue.Dequeue();
+            customQueue.Dequeue();
+            customQueue.Dequeue();
+            customQueue.Dequeue();
+            customQueue.Dequeue();
+            customQueue.Dequeue();
+            customQueue.Clear();
+            
+            Console.WriteLine(customQueue.Count);
+        }
+    }
+}
---
+++ CustomImplementationOfDataStructuresBasic/CustomQueue/Queue.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomQueue
+{
+    public class Queue<T> : IEnumerable<T>
+    {
+        private T[] elements;
+        private const int InitialCapacity = 4;
+        public Queue()
+        {
+            elements = new T[InitialCapacity];
+        }
+
+        public int Count { get; private  set; }
+
+        public void Enqueue(T item)
+        {
+            if (elements.Length==Count)
+            {
+                Resize();
+            }
+            elements[Count++] = item;
+        }
+        public T Dequeue()
+        {
+            IsEmpty();
+            Count--;
+            var firstElement = elements[0];
+            SwitchElements();
+            if (Count <= elements.Length / 4)
+            {
+                Shrink();
+            }
+            return firstElement;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in elements)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+           return GetEnumerator();
+        }
+        public T Peek()
+        {
+            return elements[0];
+        }
+        public void Clear()
+        {
+            if (Count==0)
+            {
+                throw new InvalidOperationException("The collection is empty");
+            }
+            elements = new T[InitialCapacity];
+            
+      
+        }
+
+        private void Resize()
+        {
+            T[] copyArray = new T[elements.Length * 2];
+            for (int i = 0; i < elements.Length; i++)
+            {
+                copyArray[i] = elements[i];
+            }
+            elements = copyArray;
+        }
+        private void SwitchElements()
+        {
+            elements[0] = default;
+            for (int i = 1; i < elements.Length; i++)
+            {
+                elements[i - 1] = elements[i];
+            }
+            elements[elements.Length - 1] = default;
+        }
+
+        private void IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty");
+            }
+        }
+        private void Shrink()
+        {
+            T[] copyArray = new T[elements.Length / 2];
+            for (int i = 0; i < Count; i++)
+            {
+                copyArray[i] = elements[i];
+            }
+            elements = copyArray;
+        }
+
+    }
+}
