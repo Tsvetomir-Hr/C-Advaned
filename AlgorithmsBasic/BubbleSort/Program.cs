223ad655eba9005bc58ba72f182e23603c13228d
Tsvetomir <cecohristov97@gmail.com>
2022-09-05 23:22:29 +0300

    BubbleSort

---
+++ AlgorithmsBasic/BubbleSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 7, 12, -50, 60 };
+            BubbleSort(numbers);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void BubbleSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[i])
+                    {
+                        int temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                } 
+            }
+        }
+    }
+}
