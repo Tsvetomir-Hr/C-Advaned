78d6233c648b905120137cb5d3df46a24eb7feed
Tsvetomir <cecohristov97@gmail.com>
2022-09-05 23:22:50 +0300

    SelectionSort

---
+++ AlgorithmsBasic/SelectionSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, -50, -25 };
+            SelectionSort(numbers);
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+
+        private static void SelectionSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int index = i;
+                int minNumber = int.MaxValue;
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[i] && numbers[j]<minNumber)
+                    {
+                        index = j;
+                        minNumber = numbers[j];
+                    }
+                }
+                int temp = numbers[i];
+                numbers[i] = numbers[index];
+                numbers[index] = temp;
+            }
+        }
+    }
+}
