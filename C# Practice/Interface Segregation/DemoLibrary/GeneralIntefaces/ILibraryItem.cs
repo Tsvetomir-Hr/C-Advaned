d81c4db13fe5a0389d447ccb0121cb99bf40d0ea
Tsvetomir <cecohristov97@gmail.com>
2023-01-03 23:10:21 +0200

    Interface Segregation practice

---
+++ C# Practice/Interface Segregation/DemoLibrary/GeneralIntefaces/ILibraryItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DemoLibrary
+{
+    public interface ILibraryItem
+    {
+        string LibraryId { get; set; }
+        string Title { get; set; }
+
+     
+
+ 
+    }
+}
