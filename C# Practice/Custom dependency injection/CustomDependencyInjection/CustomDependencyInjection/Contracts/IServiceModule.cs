61e16a2f6b41d765aa77d9662ff0fae60ad6749a
Tsvetomir <cecohristov97@gmail.com>
2023-01-07 23:40:10 +0200

    Custom dependency injection  - started

---
+++ C# Practice/Custom dependency injection/CustomDependencyInjection/CustomDependencyInjection/Contracts/IServiceModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDependencyInjection.Contracts
+{
+    interface IServiceModule
+    {
+        void AddSingleton<TInterface, TImplementation>();
+        void Configure();
+    }
+}
---
+++ C# Practice/Custom dependency injection/CustomDependencyInjection/CustomDependencyInjection/ServiceModule.cs
@@ -0,0 +1,26 @@
+﻿using CustomDependencyInjection.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDependencyInjection
+{
+    public abstract class ServiceModule : IServiceModule
+    {
+        Dictionary<Type, Type> mappings;
+        Dictionary<Type, object> instances;
+        public ServiceModule()
+        {
+            mappings = new Dictionary<Type, Type>();
+            instances = new Dictionary<Type, object>();
+        }
+
+        public void AddSingleton<TInterface, TImplementation>()
+        {
+            mappings[typeof(TInterface)] = typeof(TImplementation);
+        }
+
+        public abstract void Configure();
+       
+    }
+}
